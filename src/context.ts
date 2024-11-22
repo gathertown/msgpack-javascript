29a2268483ebc4099841d34b5d72471b99bdd0ea
Kumail Jaffer <mkumailjaffer@gmail.com>
2024-11-21 23:53:05 -0500

    updates to bring up-to-date (#3)

--- src/context.ts
+++ src/context.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/ban-types */
-
 export type SplitTypes<T, U> = U extends T ? (Exclude<T, U> extends never ? T : Exclude<T, U>) : T;
 
 export type SplitUndefined<T> = SplitTypes<T, undefined>;
